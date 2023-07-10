9752ec4558a0ce29fd95e3cd8bb993b99bbd9dae
Eun-SeoKim <qet98812@gmail.com>
2023-07-10 18:36:43 +0900

    commit

---
+++ src/main/java/com/dayone/Application.java
@@ -0,0 +1,18 @@
+package com.dayone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableCaching
+@ComponentScan(basePackages = {"com.dayone.persist.entity.CompanyEntity"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
